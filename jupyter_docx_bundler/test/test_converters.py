8a5f89f9919a777af84536c31ff1e70cf0430a3e
Marco Rossi <developer@marco-rossi.com>
2019-03-17 15:37:31 +0100

    Skip matplotlib testing on mac os x
    See https://github.com/matplotlib/matplotlib/issues/13096

--- jupyter_docx_bundler/test/test_converters.py
+++ jupyter_docx_bundler/test/test_converters.py
@@ -66,6 +66,10 @@ def download_notebook(request):
 
 @pytest.fixture(params=[100, 1000])
 def matplotlib_notebook(tmpdir, request):
+    # TODO Remove when matplotlib 3.04 or 3.1 is released
+    if platform.system() == 'Darwin':
+        pytest.skip('matplotlib is currently broken on Mac OS X, see https://github.com/matplotlib'
+                    '/matplotlib/issues/13096')
     nb = nbformat.v4.new_notebook()
 
     nb.cells.append(nbformat.v4.new_code_cell('\n'.join(
