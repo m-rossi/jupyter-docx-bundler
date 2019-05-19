600b066f3adb76fd51f7f13c1b8ea40de9bdf787
Marco Rossi <developer@marco-rossi.com>
2019-05-19 22:23:15 +0200

    Remove redundant calls of steps of conversion
    These functions call steps of conversion, which waste CI time. The
    steps are called during the steps anyway.

--- jupyter_docx_bundler/tests/test_converters.py
+++ jupyter_docx_bundler/tests/test_converters.py
@@ -5,37 +5,5 @@ import pytest
 from .. import converters
 
 
-def test_preprocess(tmpdir, test_notebook):
-    converters.preprocess(test_notebook)
-
-
-def test_notebook_to_html(tmpdir, test_notebook):
-    htmlfile = os.path.join(tmpdir, 'notebook.html')
-    preprocessed_notebook = converters.preprocess(test_notebook)
-    converters.notebook_to_html(preprocessed_notebook, htmlfile)
-
-
-def test_html_to_docx(tmpdir, test_notebook):
-    # convert notebook to HTML
-    htmlfile = os.path.join(tmpdir, 'notebook.html')
-    preprocessed_notebook = converters.preprocess(test_notebook)
-    converters.notebook_to_html(preprocessed_notebook, htmlfile)
-
-    # convert notebook to DOCX
-    docxfile = os.path.join(tmpdir, 'notebook.docx')
-    converters.html_to_docx(htmlfile, docxfile, metadata=preprocessed_notebook['metadata'])
-
-
-def test_html_to_docx_missing_path(tmpdir, test_notebook):
-    htmlfile = os.path.join(tmpdir, 'notebook.html')
-    docxfile = os.path.join(tmpdir, 'notebook.docx')
-    test_notebook = converters.preprocess(test_notebook)
-    converters.notebook_to_html(test_notebook, htmlfile)
-    with pytest.raises(FileNotFoundError):
-        converters.html_to_docx('not/existing.html', docxfile)
-    with pytest.raises(FileNotFoundError):
-        converters.html_to_docx(htmlfile, 'not/existing.docx')
-
-
 def test_notebookcontent_to_docxbytes(test_notebook):
     converters.notebookcontent_to_docxbytes(test_notebook, 'download-notebook')
