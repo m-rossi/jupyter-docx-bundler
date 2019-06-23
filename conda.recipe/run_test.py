108a0186dcba4e51ccfd0ddc30c9e6274e252f32
Marco Rossi <developer@marco-rossi.com>
2019-06-23 16:50:16 +0200

    Add run_test.py
    The file tests if the bundler is enabled correctly as bundlerextension and nbconvert is able to use it as an output format

---
+++ conda.recipe/run_test.py
@@ -0,0 +1,14 @@
+import re
+import subprocess
+import sys
+
+from nbconvert import nbconvertapp
+
+
+out = subprocess.run(['jupyter-bundlerextension', 'list', '--sys-prefix'], capture_output=True)
+if not re.search('Office Open XML \(\.docx\).*enable', out.stdout.decode('utf8')):
+    sys.exit(1)
+
+formats = nbconvertapp.get_export_names()
+if 'docx' not in formats:
+    sys.exit(1)
