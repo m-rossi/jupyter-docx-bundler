53d0c1a10799da9b8023e0b86d12de2d6c35e4fc
Marco Rossi <developer@marco-rossi.com>
2020-03-08 11:23:19 +0100

    Add Python 3.8 identifier

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description='Jupyter bundler extension to export notebook as a docx file',
     extras_require={
