6e17818a57889aced0de54164751b18fcf9a258c
Marco Rossi <developer@marco-rossi.com>
2020-08-06 14:11:57 +0200

    Add comments

--- jupyter_docx_bundler/tests/test_converters.py
+++ jupyter_docx_bundler/tests/test_converters.py
@@ -12,12 +12,17 @@ def test_notebookcontent_to_docxbytes(test_notebook):
 
 
 def test_image_conversion(tmpdir, images_notebook):
+    # convert notebook to docx
     docxbytes = converters.notebookcontent_to_docxbytes(
         images_notebook, 'test-notebook', images_notebook['metadata']['path'],
     )
+
+    # write to file on disk
     filename = tmpdir / 'images-notebook.docx'
     with open(filename, 'wb') as file:
         file.write(docxbytes)
+
+    # convert to markdown and extract media
     pypandoc.convert(
         f'{filename}',
         'markdown',
@@ -25,6 +30,8 @@ def test_image_conversion(tmpdir, images_notebook):
         extra_args=['--extract-media', f'{tmpdir}'],
         outputfile=f'{tmpdir / "images-notebook.md"}',
     )
+
+    # compare number of extracted media with generated media
     media = list(Path(tmpdir / 'media').glob('*.*'))
     assert images_notebook['metadata']['image_count'] == len(media),\
         'Number of generated images does not match in docx-document.'
